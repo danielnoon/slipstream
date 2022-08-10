e0d12904a3fdb0aa3e3cb817b4e483a9b08ef9c5
Andrew Herold <heroldaksd@gmail.com>
2022-08-09 19:14:15 -0500

    remove platforms 4 which course data isn't added

--- src/types/Platform.ts
+++ src/types/Platform.ts
@@ -1,12 +1,12 @@
 export enum Platform {
   Wii = "Mario Kart Wii",
-  SNES = "Super Mario Kart",
-  GBA = "Mario Kart Super Circuit",
-  GCN = "Mario Kart Double Dash!!",
+  //SNES = "Super Mario Kart",
+  //GBA = "Mario Kart Super Circuit",
+  //GCN = "Mario Kart Double Dash!!",
   Switch = "Mario Kart 8 Deluxe",
-  N64 = "Mario Kart 64",
-  WiiU = "Mario Kart 8",
-  NDS = "Mario Kart DS",
-  N3DS = "Mario Kart 7",
+  //N64 = "Mario Kart 64",
+  //WiiU = "Mario Kart 8",
+  //NDS = "Mario Kart DS",
+  //N3DS = "Mario Kart 7",
   NONE = "None"
 }
